d3aa0e43b3a6a0668f0b050b7b32fd6dcab94028
Ludovic <ludovicaubut@hotmail.com>
2026-03-28 13:16:45 -0400

    Trying width 50/100 for tune divs

--- assets/app.js
+++ assets/app.js
@@ -254,7 +254,7 @@ function updateRenderings() {
    for (let i = 0, maxI = ABC_DIV.children.length; i < maxI; i++) {
       let abcTextArea = ABC_DIV.children[i].getElementsByTagName("textarea");
       if (abcTextArea[0].value) {
-         let tuneRenderDiv = createElem(RENDERING_DIV, null, "div", null, null, SWITCH_BORDER_SETS.checked ? ["border", "border-black", "my-2", "px-1"] : null, null);
+         let tuneRenderDiv = createElem(RENDERING_DIV, null, "div", null, null, SWITCH_BORDER_SETS.checked ? ["border", "border-black", "my-2", "px-1", "w-50"] : null, null);
          if (SWITCH_RENDER_SET_NAMES.checked) {
             createElem(tuneRenderDiv, null, "div", null, null, ["fw-bold"], ABC_DIV.children[i].children[0].innerText);
          }
